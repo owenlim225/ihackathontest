7618cdd6186d2054f00b2c59ae2542ba8bf288dc
owenlim225 <owenlimosnero225@mail.com>
2024-07-20 18:19:37 +0800

    first draft

--- scripts/Control.gd
+++ scripts/Control.gd
@@ -1,10 +1,35 @@
 extends Control
+@onready var popup_quest = %PopupQuest
+@onready var popup_shop = %PopupShop
+@onready var popup_friends = %PopupFriends
+@onready var popup_ranks = %PopupRanks
+@onready var popup_settings = %PopupSettings
 
-@onready var label = $Label
-
-var count: int = 0
+@onready var coin_num_lbl = $UI/CoinNum2/CoinNumLbl
 
 
-func _on_button_pressed():
-	count += 1
-	label.text = str(count)
+var coin: int = 0
+
+# Bottom widget buttons
+func _on_btn_quest_pressed():
+	popup_quest.show()
+
+func _on_btn_shop_pressed():
+	popup_shop.show()
+
+func _on_btn_friends_pressed():
+	popup_friends.show()
+
+func _on_btn_ranks_pressed():
+	popup_ranks.show()
+
+func _on_btn_settings_pressed():
+	popup_settings.show()
+
+
+
+
+# Main tap tap function
+func _on_tap_button_pressed():
+	coin += 1
+	coin_num_lbl.text = str(coin)
